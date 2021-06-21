eaadb371fd3b8a0b4b4fd48b05003b9a31e72a0b
Ace <super.vision.dev@gmail.com>
2021-06-21 23:18:31 +0300

    chore: configure swiper responsive

--- components/landing/testimonial-section.tsx
+++ components/landing/testimonial-section.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const testimonials = [
   {
@@ -19,24 +20,43 @@ const testimonials = [
   }
 ];
 
+const swiperBreakPoints = {
+  640: {
+    width: 640,
+    slidesPerView: 1,
+  },
+  768: {
+    width: 768,
+    slidesPerView: 2,
+  },
+  1024: {
+    width: 1024,
+    slidesPerView: 3,
+  }
+};
+
 export function TestimonialSection() {
   return (<section className="pt-110 pb-20 bg-gradient-to-r from-primary-100 to-primary-75">
     <div className="container mx-auto">
       <h3 className="text-white text-32 text-center">Check out what our customers have to say</h3>
     </div>
-    <div className="px-10 sm:px-20 md:px-30 lg:px-40 xl:px-50 pb-50 pt-80">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-20">
-        {testimonials.map((testimonial, index) => (<div className="bg-white rounded-lg shadow-primary text-center relative pt-80 px-40 my-40 lg:my-0 shadow-secondary" key={index}>
-          <div className="absolute w-full flex justify-center -top-55 left-0">
-            <div className="w-110 h-110 rounded-full border-4 border-primary-50 bg-primary-50 overflow-hidden relative">
-              <Image src={testimonial.photo} layout="fill" alt={testimonial.name} objectFit="cover" />
+    <div className="px-10 sm:px-20 md:px-30 lg:px-40 xl:px-50 pb-50 pt-20">
+      <Swiper className="w-full" breakpoints={swiperBreakPoints } spaceBetween={20}>
+        {testimonials.map((testimonial, index) => (<SwiperSlide className="flex-grow" key={index}>
+          <div
+            className="bg-white rounded-lg shadow-primary text-center relative pt-80 px-40 shadow-secondary mt-55">
+            <div className="absolute w-full flex justify-center -top-55 left-0">
+              <div
+                className="w-110 h-110 rounded-full border-4 border-primary-50 bg-primary-50 overflow-hidden relative">
+                <Image src={testimonial.photo} layout="fill" alt={testimonial.name} objectFit="cover"/>
+              </div>
             </div>
+            <span className="text-warning-100 text-24 font-medium">{testimonial.title}</span>
+            <p className="pt-10 pb-20">{testimonial.words}</p>
+            <p className="text-primary text-12 pb-35">{testimonial.name}</p>
           </div>
-          <span className="text-warning-100 text-24 font-medium">{testimonial.title}</span>
-          <p className="pt-10 pb-20">{testimonial.words}</p>
-          <p className="text-primary text-12 pb-35">{testimonial.name}</p>
-        </div>))}
-      </div>
+        </SwiperSlide>))}
+      </Swiper>
     </div>
   </section>);
 }
