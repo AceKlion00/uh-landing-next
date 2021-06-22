720ce7b8d74e3fc34658818f1dc44e03e3bb3e2d
Ace <super.vision.dev@gmail.com>
2021-06-22 20:44:28 +0300

    chore: change to next script tag for google analytics

--- components/3rd-party/google-analytics.tsx
+++ components/3rd-party/google-analytics.tsx
@@ -1,7 +1,9 @@
+import Script from 'next/script';
+
 export default function GoogleAnalytics() {
   return (<>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176721306-1" />
-    <script defer dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);gtag('js',new Date());gtag('config','UA-176721306-1');` }}
+    <Script src="https://www.googletagmanager.com/gtag/js?id=UA-176721306-1" />
+    <Script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);gtag('js',new Date());gtag('config','UA-176721306-1');` }}
     />
   </>);
 }
