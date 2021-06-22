a56c8e653e02d8eb483b8b6b7369acaddafbffec
Ace <super.vision.dev@gmail.com>
2021-06-22 20:28:58 +0300

    chore: disable gzip compression

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  compress: false, // brotli compress rather than default gzip support of next.js
   env: {
     googleApiKey: 'AIzaSyDocOTLnXB8Kgn46cZSKokpsVHpq6MTORE',
     api: process.env.API_URL || 'http://127.0.0.1:3005/api',
