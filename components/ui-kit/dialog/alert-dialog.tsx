2a817a2469391ef43f4f9725c816e6d596064927
Ace <super.vision.dev@gmail.com>
2021-06-25 23:11:44 +0300

    feat: add custom actions [WIP]

--- components/ui-kit/dialog/alert-dialog.tsx
+++ components/ui-kit/dialog/alert-dialog.tsx
@@ -1,6 +1,12 @@
 import { DialogResult, DialogType } from './types';
 import Icon from '../icon';
 
+interface AlertAction {
+  caption: string;
+  onClick: () => void;
+  color: 'warning' | 'primary' | 'danger';
+}
+
 interface Props {
   title: string,
   okCaption: string,
@@ -8,9 +14,10 @@ interface Props {
   type: DialogType,
   onClose: (res: DialogResult) => void,
   closeDialog: () => void, // DO NOT USE THIS. INTERNAL USE ONLY
+  actions?: AlertAction[];
 }
 
-export function AlertDialog({ title, message, type, okCaption, onClose, closeDialog }: Props) {
+export function AlertDialog({ title, message, type, okCaption, onClose, closeDialog, actions }: Props) {
   const closeWithResult = (result: DialogResult) => {
     closeDialog();
     onClose(result);
@@ -30,6 +37,9 @@ export function AlertDialog({ title, message, type, okCaption, onClose, closeDia
         <button className="btn btn-danger btn-md mr-30" onClick={() => closeWithResult(DialogResult.Yes)}>Yes</button>
         <button className="btn btn-info btn-md" onClick={() => closeWithResult(DialogResult.No)}>No</button>
       </>)}
+      {type === DialogType.Custom && (<>
+        {actions.map((action, index) => <button className=""></button>)}
+      </>)}
     </div>
   </div>);
 }
@@ -39,4 +49,5 @@ AlertDialog.defaultProps = {
   type: DialogType.Alert,
   onClose: () => {},
   closeDialog: () => {},
+  actions: [],
 }
