57b8d61e8fafacd49f2de1fff832a3b330a26a8f
Ace <super.vision.dev@gmail.com>
2021-07-04 12:59:46 -0700

    chore: implement about us youtube video link

--- components/dialogs/video-dialog.tsx
+++ components/dialogs/video-dialog.tsx
@@ -3,11 +3,12 @@ interface Props {
 }
 export function VideoDialog({videoPath}: Props) {
   return (
-    <div className="max-w-560 max-h-315 mx-15 md:mx-0 flex items-center content-center">
-      <iframe width="100%" height="100%" src={videoPath} title="YouTube video player"
+    <div className="relative w-screen h-240 w-100w sm:h-315 sm:w-560">
+      <iframe className="absolute left-0 top-0 w-full h-full"
+              src={videoPath} title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen></iframe>
+              allowFullScreen />
     </div>
   );
 }
